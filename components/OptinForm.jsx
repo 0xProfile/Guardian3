96641461404dcd00dd913e8b0a9d02bab8043e12
Kehan Wang <wangkk@bu.edu>
2023-01-21 00:48:25 -0500

    update reporter page

---
+++ components/OptinForm.jsx
@@ -0,0 +1,111 @@
+import * as React from "react"
+import {
+    FormControl,
+    Grid,
+    Select,
+    MenuItem,
+    InputLabel,
+    Box,
+    Paper,
+    TextField,
+    Button,
+    Avatar,
+    Typography,
+} from "@mui/material"
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
+import { ConnectButton } from "@rainbow-me/rainbowkit"
+
+export default function SignInSide() {
+    const [org, setorg] = React.useState("")
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const data = new FormData(event.currentTarget)
+        console.log({
+            email: data.get("email"),
+            password: data.get("password"),
+            org: org,
+        })
+    }
+
+    const handleChange = (event) => {
+        setorg(event.target.value)
+    }
+
+    return (
+        <Grid
+            container
+            component="main"
+            sx={{
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}
+        >
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <Box
+                    sx={{
+                        my: 8,
+                        mx: 4,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Securely Opt-in Reporter List
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="name"
+                            label="Name"
+                            type="name"
+                            id="name"
+                            autoComplete="name"
+                        />
+                        <FormControl>
+                            <InputLabel id="demo-simple-select-autowidth-label">org</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-autowidth-label"
+                                id="demo-simple-select-autowidth"
+                                value={org}
+                                onChange={handleChange}
+                                autoWidth
+                                label="org"
+                                defaultValue={0}
+                            >
+                                <MenuItem value={0}>None</MenuItem>
+                                <MenuItem value={1}>The New York Times</MenuItem>
+                                <MenuItem value={2}>CoinDesk</MenuItem>
+                                <MenuItem value={3}>ChainFeeds</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+                            Sign In
+                        </Button>
+                        <ConnectButton label="Already opt-in? Connect Wallet Now!"></ConnectButton>
+                    </Box>
+                </Box>
+            </Grid>
+            {/* <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <h1>Already opt-in? Connect Wallet Now!</h1>
+            </Grid> */}
+        </Grid>
+    )
+}
--- pages/reporter/index.js
+++ pages/reporter/index.js
@@ -1,10 +1,8 @@
-import { Card } from "@mui/material"
-import React from "react"
+import * as React from "react"
+import OptinForm from "../../components/OptinForm"
+import { useAccount, useConnect } from "wagmi"
 
-export default function reporter() {
-    return (
-        <Card>
-            <div> This is reporter page</div>
-        </Card>
-    )
+export default function Reporter() {
+    const { connector: activeConnector, isConnected } = useAccount()
+    return isConnected ? <h1>{activeConnector.name}</h1> : <OptinForm></OptinForm>
 }
