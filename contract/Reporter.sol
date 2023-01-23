9a737a8492a54026cfcaecf7c730bb2069c70e95
Kehan Wang <wangkk@bu.edu>
2023-01-23 15:59:52 -0500

    Update Reporter.sol

--- contract/Reporter.sol
+++ contract/Reporter.sol
@@ -1,4 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract Reporter {}
+struct Reporter {
+    //Declaring different structure elements
+    string name;
+    string email;
+    string org;
+}
+
+contract ReporterOperator {
+    mapping(address => Reporter) reporterAccounts;
+    Reporter[] public reporterAccountList;
+
+    function add(
+        address _address,
+        string memory _name,
+        string memory _email,
+        string memory _org
+    ) public {
+        Reporter memory curr = Reporter(_name, _email, _org);
+        reporterAccounts[_address] = curr;
+        reporterAccountList.push(curr);
+    }
+
+    function getReporterInfoWithAddress(address _address) public view returns (Reporter memory) {
+        return reporterAccounts[_address];
+    }
+
+    function getReporterAccounts() public view returns (Reporter[] memory) {
+        return reporterAccountList;
+    }
+}
