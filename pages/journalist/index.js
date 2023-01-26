ad294aa97bcf9a3d057ec254088064b2279c622f
Kehan Wang <wangkk@bu.edu>
2023-01-26 00:56:57 -0500

    Update use of key phrase

--- pages/journalist/index.js
+++ pages/journalist/index.js
@@ -1,34 +1,29 @@
 import * as React from "react"
 import dynamic from "next/dynamic"
-import {reporterAddr} from '../../constants'
-import reporterABI from '../../constants/abis/reporter.json'
+import { reporterAddr } from "../../constants"
+import reporterABI from "../../constants/abis/reporter.json"
 const OptinForm = dynamic(() => import("../../components/OptinForm"), { ssr: false })
 const OptinCard = dynamic(() => import("../../components/OptinCard"), { ssr: false })
 const Inbox = dynamic(() => import("../../components/Inbox"), { ssr: false })
-const ConnectButton = dynamic(() => import("@rainbow-me/rainbowkit"), { ssr: false })
 
 import { useAccount, useSigner, useContractRead } from "wagmi"
 
 export default function Reporter() {
     const { connector, isConnected, address } = useAccount()
 
-    const { data: isOptin=false } = useContractRead({
+    const { data: isOptin = false } = useContractRead({
         address: reporterAddr,
         abi: reporterABI,
         functionName: "isOptedIn",
-        args: [address]
+        args: [address],
     })
 
-    return isConnected ? (
-        isOptin ? (
-            <div>
-                <OptinCard></OptinCard>
-                <Inbox></Inbox>
-            </div>
-        ) : (
-            <OptinForm></OptinForm>
-        )
+    return isOptin ? (
+        <div>
+            <OptinCard></OptinCard>
+            <Inbox></Inbox>
+        </div>
     ) : (
-        <ConnectButton label="Please Connect Wallet to start!"></ConnectButton>
+        <OptinForm></OptinForm>
     )
 }
